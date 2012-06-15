7981c69644dc1e736ea6648ac410d31502dc0b87
Erica Mazza <erica.mazza1@gmail.com>
2012-06-15 00:14:41 -0500

    global style changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,6 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
+
+<script type="text/javascript" src="http://use.typekit.com/zle5lye.js"></script>
+<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
